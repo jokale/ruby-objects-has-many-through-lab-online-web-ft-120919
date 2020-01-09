c7c59d5530bdca7b955bc50490c505bdc56872a2
Joanna E K <kalemajoanna@gmail.com>
2020-01-09 08:56:35 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -23,7 +23,7 @@ class Doctor
     end
 
    def patients
-      Appointment.all.select  |appointment|
+      Appointment.all.collect do  |appointment|
         appointment.patient
       end
     # end
